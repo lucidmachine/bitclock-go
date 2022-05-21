7cee56640b61dd6ecc93b989b3e4ed02fd6cd1f7
Eric Slenk <lucid.machine@gmail.com>
2022-05-21 17:54:32 -0400

    Render in event loop with tcell

--- main.go
+++ main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"time"
 
-	backend "github.com/lucidmachine/bitclock-go/pkg/backend"
+	tc "github.com/gdamore/tcell/v2"
+
+	"github.com/lucidmachine/bitclock-go/pkg/backend"
+	"github.com/lucidmachine/bitclock-go/pkg/frontend"
 )
 
+func handleEvent(screen tc.Screen, ev tc.Event) {
+	switch ev := ev.(type) {
+
+	case *tc.EventResize:
+		screen.Sync()
+
+	case *tc.EventKey:
+		if ev.Key() == tc.KeyEscape || ev.Key() == tc.KeyCtrlC {
+			screen.Fini()
+			os.Exit(0)
+		}
+	}
+}
+
 func main() {
-	now := time.Now()
-	bc := backend.NewBitTime(now)
-	fmt.Println(bc)
+	screen, err := tc.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	frontend.Init(screen)
+
+	for {
+		// Build and display time
+		now := time.Now()
+		bt := backend.NewBitTime(now)
+		frontend.Render(screen, bt)
+
+		// Handle events
+		if screen.HasPendingEvent() {
+			ev := screen.PollEvent()
+			handleEvent(screen, ev)
+		}
+
+		// Wait before next render
+		time.Sleep(1 * time.Second)
+	}
 }
--- pkg/frontend/frontend.go
+++ pkg/frontend/frontend.go
@@ -1,11 +1,35 @@
 package frontend
 
 import (
-	"fmt"
+	"log"
 
-	backend "github.com/lucidmachine/bitclock-go/pkg/backend"
+	tc "github.com/gdamore/tcell/v2"
+
+	"github.com/lucidmachine/bitclock-go/pkg/backend"
 )
 
-func render(bt backend.BitTime) {
-	fmt.Println(bt)
+func style(bit int) tc.Style {
+	if bit > 0 {
+		return tc.StyleDefault.Background(tc.ColorWhite)
+	} else {
+		return tc.StyleDefault.Background(tc.ColorBlack)
+	}
+}
+
+func Init(screen tc.Screen) {
+	if err := screen.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	screen.SetStyle(tc.StyleDefault.Background(tc.ColorBlack).Foreground(tc.ColorWhite))
+	screen.Clear()
+}
+
+func Render(screen tc.Screen, bt backend.BitTime) {
+	for x := 0; x < 6; x++ {
+		for y := 0; y < 4; y++ {
+			screen.SetContent(x, y, ' ', nil, style(bt[x][y]))
+		}
+	}
+
+	screen.Show()
 }
